122e14223dff29c4791fd74e070e9af1b4f13508
PAULMURILLOO <pauljomarmurillo@gmail.com>
2024-03-10 20:28:59 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Murillo_My_First_Application"
+include(":app")
+ 
